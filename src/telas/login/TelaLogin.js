7636fbbfaf1e230d514d110d349c04aa3dd0f188
JKalashinikov <jaedsonbruno@gmail.com>
2017-08-18 23:53:29 -0300

    Login e Logout funcionando com persistencia de dados usando AsyncStorage

--- src/telas/login/TelaLogin.js
+++ src/telas/login/TelaLogin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, View, StatusBar, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { ScrollView, View, StatusBar, TouchableOpacity, KeyboardAvoidingView, AsyncStorage } from 'react-native';
 import {
     FormLabel,
     FormInput,
@@ -8,7 +8,7 @@ import {
     SocialIcon,
     Text
 } from 'react-native-elements';
-import FBSDK, {LoginManager, LoginButton, AccessToken } from 'react-native-fbsdk';
+import FBSDK, {LoginManager, LoginButton, AccessToken, GraphRequestManager, GraphRequest } from 'react-native-fbsdk';
 import styles from 'DietaViverSaudavel/src/styles/Styles';
 import PropTypes from 'prop-types';
 //import FormLogin from 'DietaViverSaudavel/src/components/FormLogin';
@@ -28,6 +28,30 @@ export default class TelaLogin extends React.Component {
     static navigationOptions = {
         title: 'Login',
     };
+
+    async saveInfo(result){
+      console.log("result")
+      AsyncStorage.setItem('first_name', result['first_name']);
+      AsyncStorage.setItem('last_name', result['last_name']);
+      try{
+         let a = await AsyncStorage.getItem('first_name');
+         console.log(a)
+      }catch(erro){
+
+      }
+    }
+
+    getAllInfo(result){
+      AsyncStorage.getAllKeys((err, keys) => {
+      AsyncStorage.multiGet(keys, (err, stores) => {
+       stores.map((result, i, store) => {
+         // get at each store's key/value so you can work with it
+         let key = store[i][0];
+         let value = store[i][1];
+        });
+      });
+    });
+    }
 
     render() {
         return (
@@ -78,9 +102,43 @@ export default class TelaLogin extends React.Component {
                             } else {
                               AccessToken.getCurrentAccessToken().then(
                                 (data) => {
-                                  alert(data.accessToken.toString())
-                                }
-                              )
+                                  let accessToken = data.accessToken;
+                                  alert(accessToken.toString());
+
+                                  const responseInfoCallback = (error, result) => {
+                                    if (error) {
+                                      console.log(error)
+                                      alert('Error fetching data: ' + error.toString());
+                                    } else {
+                                      console.log(result)
+                                      console.log(result['first_name'])
+                                      try {
+                                        console.log("Aqui!")
+                                        this.saveInfo(result);
+                                      } catch (error) {
+                                      // Error saving data
+                                      }
+                                      alert('Success fetching data: ' + result.toString());
+                                    }
+                                  }
+
+                                  const infoRequest = new GraphRequest(
+                                    '/me',
+                                    {
+                                      accessToken: accessToken,
+                                      parameters: {
+                                        fields: {
+                                          string: 'email,name,first_name,middle_name,last_name'
+                                        }
+                                      }
+                                    },
+                                    responseInfoCallback
+                                  );
+
+                                  // Start the graph request.
+                                  new GraphRequestManager().addRequest(infoRequest).start();
+
+                                })
                             }
                           }
                         }
