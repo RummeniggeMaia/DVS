a31bf819368625b80607ab5563278597792745c9
JKalashinikov <jaedsonbruno@gmail.com>
2017-09-01 01:41:56 -0300

    Começando a programar a alteração de senha

--- src/telas/login/TelaAlterarSenha.js
+++ src/telas/login/TelaAlterarSenha.js
@@ -12,14 +12,99 @@ import styles from 'DietaViverSaudavel/src/styles/Boxes_style.js';
 export default class TelaAlterarSenha extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { textSenhaAtual: 'Insira sua senha atual.',
-                   textRepetirSenha: 'Repita sua senha.',
-                   textNovaSenha: 'Insira sua nova senha.'
+    this.state = { senhaAtual: "",
+                   confirmarSenha: "",
+                   senhaNova: "",
+                   carregou: true
                  };
   }
     static navigationOptions = {
         title: 'Alterar Senha',
     };
+
+    async alterarSenha() {
+
+        if (!this.validar()) {
+            return;
+        }
+        this.setState({carregou: false});
+        let senha = hash.sha256().update(this.state.senhaAtual).digest('hex');
+        let novaSenha = hash.sha256().update(this.state.senhaNova).digest('hex');
+        let usuarioNome = AsyncStorage.getItem(Util.USUARIO).then("nome");
+        let usuarioEmail = AsyncStorage.getItem(Util.USUARIO).then("email");
+
+        await fetch(Util.SERVIDOR_URL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                nome: usuarioNome,
+                email: usuarioEmail,
+                senha: senhaAtual,
+                novaSenha: novaSenha,
+                func: "alterar_senha"
+            })
+        }).then((response) => response.json())
+        .then((responseJson) =>{
+            if (responseJson.resp.status == "ok") {
+                let usuario = JSON.stringify({
+                    nome: usuarioNome;
+                    email: usuarioEmail;
+                    senha: novaSenha
+                });
+                let res = true;
+                AsyncStorage.setItem(Util.USUARIO, usuario).catch((error) => {
+                    res = false;
+                });
+                if (res) {
+                    const resetAction = NavigationActions.reset({
+                        index: 0,
+                        actions: [
+                            NavigationActions.navigate(
+                                {
+                                    routeName: 'DadosPessoais',
+                                    params:{primeiraVez: true}
+                                }
+                            )
+                        ],
+                    });
+                    this.props.navigation.dispatch(resetAction);
+                } else {
+                    Alert.alert("Falha no registro", "Não pode salvar dados.");
+                }
+            }
+        }).catch((error) => {
+            Alert.alert("Falha no registro", "Falha no servidor.");
+        }).done(() => {
+            this.setState({carregou: true});
+        });
+    }
+
+    validar () {
+        if (this.state.senhaAtual === "") {
+            Alert.alert("Falha no registro!", "Insira uma senha.");
+            return false;
+        }
+        if (this.state.confirmarSenha === "") {
+            Alert.alert("Falha no registro!", "Repita a nova senha.");
+            return false;
+        }
+        if (this.state.senhaNova === "") {
+            Alert.alert("Falha no registro!", "Insira a senha nova.");
+            return false;
+        }
+        if (this.state.senhaAtual === "" || this.state.confirmarSenha === "") {
+            Alert.alert("Falha no registro!", "Insira uma senha.");
+            return false;
+        } else if (this.state.senhaNova !== this.state.confirmarSenha) {
+            Alert.alert("Falha no registro!", "Senhas não coincidem.");
+            return false;
+        }
+        return true;
+    }
+
     render() {
         return (
           <ScrollView
@@ -35,7 +120,7 @@ export default class TelaAlterarSenha extends React.Component {
                           style={{height: 40, borderColor: 'gray', borderWidth: 0}}
                           autoCapitalize="none"
                           secureTextEntry={true}
-                          placeholder={this.state.textSenhaAtual}
+                          placeholder={this.state.senhaAtual}
                         />
                   <FormLabel>
                     Nova Senha:
@@ -44,7 +129,7 @@ export default class TelaAlterarSenha extends React.Component {
                           style={{height: 40, borderColor: 'gray', borderWidth: 0}}
                           autoCapitalize="none"
                           secureTextEntry={true}
-                          placeholder={this.state.textNovaSenha}
+                          placeholder={this.state.senhaNova}
                         />
                   <FormLabel>
                     Repetir Senha:
@@ -53,7 +138,7 @@ export default class TelaAlterarSenha extends React.Component {
                           style={{height: 40, borderColor: 'gray', borderWidth: 0}}
                           autoCapitalize="none"
                           secureTextEntry={true}
-                          placeholder={this.state.textRepetirSenha}
+                          placeholder={this.state.confirmarSenha}
                         />
                   <Button
                       onPress = { () => this.props.navigation.navigate('Alimentacao') }
@@ -62,7 +147,7 @@ export default class TelaAlterarSenha extends React.Component {
                   <View style={styles.line}></View>
                   <Button
                       onPress = { () => this.props.navigation.navigate('Alimentacao') }
-                      title = "Mudar Senha"
+                      title = "Alterar Senha"
                       backgroundColor = "#5497FF"/ >
             </View>
           </ScrollView>
--- src/util/Util.js
+++ src/util/Util.js
@@ -1,5 +1,5 @@
 export default class Util {
-    static SERVIDOR_URL = "http://192.168.0.104/dvs/dvs_ctrl.php";
+    static SERVIDOR_URL = "http://192.168.0.103/dvs/dvs_ctrl.php";
 	//static SERVIDOR_URL = "http://dietaviversaudavel.com/dvs_rest/dvs_ctrl.php";
     static USUARIO = 'dvs_user';
 };
